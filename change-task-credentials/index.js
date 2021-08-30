84373732e4b35b5181a7226a1d41cb29c6ed1808
DaniilShmelev <v-dshmelev@microsoft.com>
2021-08-30 11:02:01 +0300

    accout for existing tasks

--- change-task-credentials/index.js
+++ change-task-credentials/index.js
@@ -13,6 +13,12 @@ if (taskName === undefined) {
   throw new Error('Task name must be specified');
 }
 
+const existingTasks = JSON.parse(fs.readFileSync('./tasks.json'));
+function getTaskGuid(taskName) {
+  const existingTask = existingTasks.find((task) => task.name === taskName);
+  return existingTask?.id;
+}
+
 const tasksFolders = fs.readdirSync(path.join(repoPath, 'Tasks'));
 tasksFolders.forEach((taskFolder) => {
   if (taskFolder !== taskName) {
@@ -22,7 +28,7 @@ tasksFolders.forEach((taskFolder) => {
   const fullTaskJsonPath = path.join(repoPath, 'Tasks', taskFolder, 'task.json');
   const taskJson = JSON.parse(fs.readFileSync(fullTaskJsonPath));
   
-  taskJson.id = guid();
+  taskJson.id = getTaskGuid(taskJson.name) ?? guid();
   taskJson.name = `${taskJson.name}M`;
   taskJson.friendlyName = `${taskJson.friendlyName}M`;
   taskJson.description = `DANIIL SHMELEVS TASK DON'T TOUCH`;
